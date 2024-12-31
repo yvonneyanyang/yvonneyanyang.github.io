76b1cb5634577213bbb106f13ef569297ea78eb7
yvonneyanyang <yuweny4@illinois.edu>
2024-12-31 08:04:41 +0000

    Deploying to gh-pages from @ yvonneyanyang/yvonneyanyang.github.io@2356c3fa641b94c1cac8533f6f4393dce4431f30 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -30,13 +30,6 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/cv/";
           },
-        },{id: "nav-people",
-          title: "people",
-          description: "members of the lab or group",
-          section: "Navigation",
-          handler: () => {
-            window.location.href = "/people/";
-          },
         },{id: "dropdown-publications",
               title: "publications",
               description: "",
